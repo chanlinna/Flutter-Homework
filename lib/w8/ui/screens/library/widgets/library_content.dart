37c2aa16b4f50bf2adfa729bb373aa4ff696c720
chanlinna <chanlinnalin@gmail.com>
2026-03-12 00:12:03 +0700

    forgot to import flutter_riverpod

--- lib/w8/ui/screens/library/widgets/library_content.dart
+++ lib/w8/ui/screens/library/widgets/library_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider/provider.dart';
 import '../../../theme/theme.dart';
 import '../../../widgets/song/song_tile.dart';
