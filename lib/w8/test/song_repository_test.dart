695db117e27325437824b04a3f3ac2bdc0bdbd17
chanlinna <chanlinnalin@gmail.com>
2026-03-11 11:07:12 +0700

    create test case for fetchSongById function

--- lib/w8/test/song_repository_test.dart
+++ lib/w8/test/song_repository_test.dart
@@ -1,10 +1,48 @@
- 
+import 'package:flutter_homework/w8/data/repositories/songs/song_repository_mock.dart';
+
 void main() async {
   //   Instantiate the  song_repository_mock
- 
+  final songRepoMock = SongRepositoryMock();
+
   // Test both the success and the failure of the post request
- 
+
   // Handle the Future using 2 ways  (2 tests)
   // - Using then() with .catchError().
+  void testWithThenCatch() {
+    songRepoMock.fetchSongById("s1")
+        .then((song) {
+          print("success: ${song?.title}");
+        })
+        .catchError((error) {
+          print(error);
+        });
+
+    songRepoMock.fetchSongById("s6")
+        .then((song) {
+          print("success: ${song?.title}");
+        })
+        .catchError((error) {
+          print(error);
+        });
+  }
+
   // - Using async/await with try/catch.
+  void testWithAsyncAwait() async {
+    try {
+      final song = await songRepoMock.fetchSongById("s1");
+      print("success: ${song?.title}");
+    } catch (error) {
+      print(error);
+    }
+
+    try {
+      final song = await songRepoMock.fetchSongById("s6");
+      print("success: ${song?.title}");
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  testWithThenCatch();
+  testWithAsyncAwait();
 }
