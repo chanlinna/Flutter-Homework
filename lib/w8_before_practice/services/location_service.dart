3bd9ecfd598740f93c63c1192e3850e4065db3d9
chanlinna <chanlinnalin@gmail.com>
2026-03-12 21:31:02 +0700

    Start code

---
+++ lib/w8_before_practice/services/location_service.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_homework/w8_before_practice/data/dummy_data.dart';
+
+import '../model/ride/locations.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; // TODO for now fake data
+}
