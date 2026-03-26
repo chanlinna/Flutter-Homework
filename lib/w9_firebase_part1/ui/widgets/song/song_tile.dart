b85968f1069da8f598f8c2cb303c4b9bf2b9d5b6
chanlinna <chanlinnalin@gmail.com>
2026-03-26 19:56:28 +0700

    w9-01-update songTile to display the image

--- lib/w9_firebase_part1/ui/widgets/song/song_tile.dart
+++ lib/w9_firebase_part1/ui/widgets/song/song_tile.dart
@@ -24,6 +24,9 @@ class SongTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(15)
         ),
         child: ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(song.imageUrl.toString()),
+          ),
           onTap: onTap,
           title: Text(song.title),
           trailing: Text(
