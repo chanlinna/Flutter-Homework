82c625bf540c896bf49c6792e0021f09738acb98
chanlinna <chanlinnalin@gmail.com>
2026-04-02 23:50:55 +0700

    W10-03 - create comment DTO

---
+++ lib/w10_practice_firebase_part2/data/dtos/comment_dto.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_homework/w10_practice_firebase_part2/model/comment/comment.dart';
+
+class CommentDto {
+  static const String artistIdKey = 'artistId';
+  static const String textKey = 'text';
+
+  static Comment fromJson(String id, Map<String, dynamic> json) {
+    assert(json[artistIdKey] is String);
+    assert(json[textKey] is String);
+
+    return Comment(id: id, artistId: json[artistIdKey], text: json[textKey]);
+  }
+
+  Map<String, dynamic> toJson(Comment comment) {
+    return {artistIdKey: comment.artistId, textKey: comment.text};
+  }
+}
